6f4ee53ccd2ed53f054dd07b670937983bae89cf
Ken Parker <kenparkerjr@yahoo.com>
2019-08-26 22:11:12 -0500

    Remove parse options and refactor Tokenizer

--- Calc/Interfaces/IParseOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Calc
-{
-    public interface IParseOptions
-    {
-        int? MaximumNumbers { get; }
-        bool ShouldDenyNegativeNumbers { get; }
-        long UpperBound { get; }
-    }
-}
